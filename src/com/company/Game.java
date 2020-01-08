cb7dea11147e177cebb95bc28cbe58dd72134dfd
JorisBaudchon <55876397+JorisBaudchon@users.noreply.github.com>
2020-01-08 19:01:39 +0100

    newDepart

---
+++ src/com/company/Game.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Game {
+    private numberOfPlayers;
+    private List<Player> :players;
+
+    public void InitGame() {
+        System.out.println("Bienvenue au jeu ROBOT TURTLE !");
+
+    }
+
+    private boolean NewGame() {
+        System.out.println("Souhaitez vous faire une partie de ROBOT TURTLE ?");
+        do {
+            entre = sc.nextline();
+
+        }
+    }
+
+}
