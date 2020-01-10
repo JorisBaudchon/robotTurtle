9c8943b1856104d15bc8c6a1fe78c48db7eb7c26
AntoineDrrs <antoine.darras@isep.fr>
2020-01-10 19:52:32 +0100

    v16

---
+++ src/com/company/GraphicalUserInterface.java
@@ -0,0 +1,9 @@
+package com.company;
+import javax.swing.JFrame;
+
+public class GraphicalUserInterface {
+    public static void main(String[] args){
+        JFrame fenetre = new JFrame();
+        fenetre.setVisible(true);
+    }
+}
