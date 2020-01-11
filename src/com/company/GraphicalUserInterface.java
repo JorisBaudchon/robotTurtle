e8ac3d33e5c80e2abf116ea2325e2ccc46d8a017
Arpit235 <58813391+Arpit235@users.noreply.github.com>
2020-01-11 17:13:30 +0100

    Update GraphicalUserInterface.java

--- src/com/company/GraphicalUserInterface.java
+++ src/com/company/GraphicalUserInterface.java
@@ -6,7 +6,13 @@ import java.awt.*;
 
 
 public class GraphicalUserInterface extends JFrame{
+    
     public static void main(String[] args) {
+        
+    }
+    
+    public GraphicalUserInterface(){
+        
         JFrame window = new JFrame();
         window.setTitle("Robot Turtle");
         window.setResizable(false);
@@ -15,5 +21,41 @@ public class GraphicalUserInterface extends JFrame{
         window.pack();
         window.setSize(1000, 700);
         window.setAlwaysOnTop(true);
-    }
+        window.setLocationRelativeTo(null);
+        window.getContentPane().setLayout(null);
+        
+        JLabel titreConteneur = new JLabel("ROBOT TURTLES");
+       titreConteneur.setBounds(500
+               , 43, 300, 20);
+       window.getContentPane().add(titreConteneur);
+
+       JButton buttonFinish = new JButton("J'ai fini");
+       buttonFinish.setBounds(871, 624, 115, 30);
+       buttonFinish.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+       window.getContentPane().add(buttonFinish);
+
+       JButton BlueCard = new JButton("Avancer");
+       BlueCard.setBounds(51, 500, 100, 155);
+       window.getContentPane().add(BlueCard);
+
+       JButton PurpleCard = new JButton("Gauche");
+       PurpleCard.setBounds(133, 500, 100, 155);
+       window.getContentPane().add(PurpleCard);
+
+       JButton YellowCard = new JButton("Droite");
+       YellowCard.setBounds(215, 500, 100, 155);
+       fenetre.getContentPane().add(YellowCard);
+
+       JButton LaserCard = new JButton("Laser");
+       LaserCard.setBounds(296, 500, 100, 155);
+       window.getContentPane().add(LaserCard);
+
+       JButton Defausse = new JButton("Defausser");
+       Defausse.setBounds(378, 500, 100, 155);
+       window.getContentPane().add(Defausse);
+
+       JButton btnValidCard = new JButton("Valider la carte");
+       btnValidCard.setBounds(500, 624, 150, 30);
+       window.getContentPane().add(btnValidCard);
+        
 }
