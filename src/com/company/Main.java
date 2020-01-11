bcaccae6c24b6319da9cc59b5efbbca604a1c70d
Arpit235 <58813391+Arpit235@users.noreply.github.com>
2020-01-11 12:45:16 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,12 @@
 package com.company;
 
 public class Main {
+    
+    public static GraficalUserInterface gui;
+    
     public static void main(String[] args) {
+        
+        gui = new GraficalUserInterface();
         Game Game = new Game();
         Game.initGame();
     }
